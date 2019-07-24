7f7146ae322918d8bb8c11242651e2685fec4e64
Andrew Zhang <and.zhang9.8@gmail.com>
2019-07-24 13:53:25 -0700

    tower now spawn every 5 s

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -45,7 +45,7 @@ public class GameController : MonoBehaviour
         }
         else if (currentState == GAMESTATE.ON_PROGRESS)
         {
-            if ((Time.time - time) / 1 > towerNumber)
+            if ((Time.time - time) / 5 > towerNumber)
             {
                 SpawnTowers();
                 towerNumber++;
